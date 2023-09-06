f12d6f01579bf502d3c26a5836ec7fdcd7dae0fd
Bahare <itxbb09@gmail.com>
2023-09-06 21:51:36 +0100

    linkedin and git hub links

--- portfolio/src/components/banner/Media.jsx
+++ portfolio/src/components/banner/Media.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import { FaLinkedinIn, FaReact, FaGithub } from "react-icons/fa";
 import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+
 const Media = () => {
+  // Define your LinkedIn and GitHub URLs
+  const linkedinUrl =
+    "https://www.linkedin.com/in/bahare-bahreinian-002843246/";
+  const githubUrl = "https://github.com/Bahare09";
+
   return (
     <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
       <div>
         <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
         <div className="flex gap-4">
-          <span className="bannerIcon">
-            <FaGithub />
-          </span>
-          <span className="bannerIcon">
+          {/* Create a link to your LinkedIn profile */}
+          <a
+            href={linkedinUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bannerIcon"
+          >
             <FaLinkedinIn />
-          </span>
+          </a>
+          {/* Create a link to your GitHub profile */}
+          <a
+            href={githubUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bannerIcon"
+          >
+            <FaGithub />
+          </a>
         </div>
       </div>
       <div>
