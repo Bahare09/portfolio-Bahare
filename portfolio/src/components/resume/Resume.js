daac843871fc4c7ea3380b46e4fd82265737ab0e
Bahare <itxbb09@gmail.com>
2023-09-08 16:07:47 +0100

    add cv button

--- portfolio/src/components/resume/Resume.js
+++ portfolio/src/components/resume/Resume.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { FaFilePdf } from "react-icons/fa"; // Import the PDF icon from react-icons/fa
 import Title from "../layouts/Title";
 
 const Resume = () => {
+  const handleCVButtonClick = () => {
+    // Handle what happens when the CV button is clicked
+    window.open(
+      "https://drive.google.com/file/d/1oppDxewLKmHTvPQfgEPZsnGEFpOWAqAq/view?usp=sharing",
+      "_blank",
+      "noopener noreferrer"
+    );
+  };
+
   return (
     <section id="resume" className="w-full py-20 border-b-[1px] border-b-black">
       <div className="flex justify-center items-center text-center">
@@ -18,15 +28,13 @@ const Resume = () => {
           believe that teamwork enhances the impact of web experiences. I'm
           excited about the endless possibilities in web development and look
           forward to creating solutions that inspire and empower.{" "}
-          <a
-            href="/path-to-your-cv.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-designColor hover:underline"
+          <button
+            className="text-white bg-designColor hover:bg-opacity-80 rounded-full py-2 px-4 transition duration-300 ease-in-out cursor-pointer flex items-center"
+            onClick={handleCVButtonClick}
+            style={{ marginTop: "150px", marginLeft: "570px" }}
           >
-            here
-          </a>
-          .
+            <FaFilePdf className="mr-2" /> View CV
+          </button>
         </p>
       </div>
     </section>
