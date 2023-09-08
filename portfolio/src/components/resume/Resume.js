db55dc6df4e5c934394dc4692aaea16a479a7f74
Bahare <itxbb09@gmail.com>
2023-09-08 14:15:05 +0100

    resume.js update

--- portfolio/src/components/resume/Resume.js
+++ portfolio/src/components/resume/Resume.js
@@ -9,8 +9,15 @@ const Resume = () => {
       </div>
       <div className="text-center mt-8">
         <p className="text-lg text-gray-400">
-          I am an experienced software developer with a passion for creating
-          innovative solutions. You can check out my CV{" "}
+          I'm a passionate web developer with a background in problem-solving
+          and technology. My journey into web development has been driven by a
+          thirst for creative solutions and a commitment to lifelong learning.
+          Equipped with a unique skill set that includes research, coding, and
+          data modeling, I'm dedicated to crafting innovative web solutions. My
+          approach is rooted in effective communication and collaboration, as I
+          believe that teamwork enhances the impact of web experiences. I'm
+          excited about the endless possibilities in web development and look
+          forward to creating solutions that inspire and empower.{" "}
           <a
             href="/path-to-your-cv.pdf"
             target="_blank"
