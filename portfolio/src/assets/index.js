44254ad1c745ade1afcf139be3ba0c76bf82b852
Bahare <itxbb09@gmail.com>
2023-10-22 23:03:26 +0100

    projectone image changed

--- portfolio/src/assets/index.js
+++ portfolio/src/assets/index.js
@@ -1,6 +1,6 @@
 import logo from "./images/logo.jpg";
 import bannerImg from "./images/bannerImg.jpg";
-import projectOne from "./images/projects/projectOne.jpg";
+import projectOne from "./images/projects/projectOne.png";
 import projectTwo from "./images/projects/projectTwo.png";
 import projectThree from "./images/projects/projectThree.png";
 import contactImg from "./images/contact/contactImg.png";
