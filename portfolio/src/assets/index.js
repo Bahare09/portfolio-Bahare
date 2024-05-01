c6654dffe398cb7af489890a7110fd8731026925
Bahare <itxbb09@gmail.com>
2024-05-01 09:46:42 +0100

    add new project

--- portfolio/src/assets/index.js
+++ portfolio/src/assets/index.js
@@ -1,8 +1,18 @@
 import logo from "./images/logo.jpg";
 import bannerImg from "./images/bannerImg.jpg";
 import projectOne from "./images/projects/projectOne.png";
-import projectTwo from "./images/projects/projectTwo.png";
+import projectFour from "./images/projects/projectFour.png";
 import projectThree from "./images/projects/projectThree.png";
+import projectTwo from "./images/projects/projectTwo.png";
+
 import contactImg from "./images/contact/contactImg.png";
 
-export { logo, bannerImg, projectOne, projectTwo, projectThree, contactImg };
+export {
+  logo,
+  bannerImg,
+  projectOne,
+  projectTwo,
+  projectThree,
+  projectFour,
+  contactImg,
+};
--- portfolio/src/components/projects/Projects.js
+++ portfolio/src/components/projects/Projects.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Title from "../layouts/Title";
-import { projectOne, projectTwo, projectThree } from "../../assets/index";
+import {
+  projectOne,
+  projectTwo,
+  projectThree,
+  projectFour,
+} from "../../assets/index";
 import ProjectsCard from "./ProjectsCard";
 
 const Projects = () => {
@@ -14,6 +19,13 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
+          title="ApplySmart"
+          des=" A personalised CV and cover letter creation solution, overseeing the development process and spearheading prompt engineering for seamless integration with the OpenAI API. Employing technologies including React, Express, and PostgreSQL, the project delivered efficient full-stack functionality"
+          src={projectTwo}
+          githubLink="https://github.com/Bahare09/ApplySmart"
+          webLink="https://applysmartc.onrender.com/"
+        />
+        <ProjectsCard
           title="WordWise"
           des="A full-stack web application was developed using React, Node.js, and Postgresql, with an  integrated open AI API for precise phrase correction. "
           src={projectOne}
@@ -23,7 +35,7 @@ const Projects = () => {
         <ProjectsCard
           title="Rock Paper Scissors"
           des="This is a team-work approach for Rock-Paper-Scissors Online Multiplayer Game implemented using React.js in Front-end and Node.js in Back-end. The main challenge was to enable instant communication between 2 players without any delays. Therefore, Socket.io library is used to enable this feature."
-          src={projectTwo}
+          src={projectFour}
           githubLink="https://github.com/Bahare09/team-game-rock-paper-scissors"
           webLink="https://rock-paper-scissors-team.netlify.app/"
         />
@@ -33,13 +45,6 @@ const Projects = () => {
           src={projectThree}
           githubLink="https://github.com/Bahare09/Full-Stack-Project-Assessment"
           webLink="https://video-recommandation-4.onrender.com/"
-        />
-       <ProjectsCard
-          title="ApplySmart"
-          des=" A personalised CV and cover letter creation solution, overseeing the development process and spearheading prompt engineering for seamless integration with the OpenAI API. Employing technologies including React, Express, and PostgreSQL, the project delivered efficient full-stack functionality"
-          src={projectfour}
-          githubLink="https://github.com/Bahare09/ApplySmart"
-          webLink="https://applysmartc.onrender.com/"
         />
       </div>
     </section>
